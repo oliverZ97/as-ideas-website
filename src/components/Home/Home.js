a9df3413b2f5f03b6326262bd1cda91ab82a897e
Andreas Wagner <andreas.wagner@axelspringer.com>
2017-09-08 16:47:36 +0200

    Add some more texts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,13 +20,17 @@ const MainNav = () => (
 
 const HeroText = () => {
   return (
-    <div className="hero__text">
-      <h1 className="hero-heading has-cursor">
-        We drive Axel Springer's digitalization 
-      </h1>
-      <p className="hero-intro">
-        And we can be cool for you too
-      </p>
+    <div className="l-container">
+      <div className="grid">
+        <div className="col-8">
+          <h1 className="hero-heading has-cursor">
+            We drive Axel Springer's digitalization 
+          </h1>
+          <p className="hero-intro">
+            And we can be cool for you too
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
@@ -56,27 +60,98 @@ const IdeasIntro = () => (
 );
 
 
-const Products = () => (
+const ProductNewsfinder = () => (
   <div className="l-container">
-  <div className="grid">
-    <div className="col-4">
-      <h2 className="h1 has-cursor has-cursor--400">
-        Newsfinder
-      </h2>
-      <p className="intro">
-        We empower editors to react faster and better to the current issues.
-      </p>
-    </div>
-    <div className="col-5" data-push-left="off-1_xs-0">
-      <p className="product-intro">
-        Editors can create their own dashboard for a thematic search through their own or third-party publications. Google Trends, an article preview and the translation of all articles into English at the push of a button round off our service for the editor of today.
-      </p>
-      <p className="product-intro">
-        Editors can create their own dashboard for a thematic search through their own or third-party publications. Google Trends, an article preview and the translation of all articles into English at the push of a button round off our service for the editor of today.
-      </p>
+    <div className="grid">
+      <div className="col-4">
+        <h2 className="h1 has-cursor has-cursor--400">
+          Newsfinder
+        </h2>
+        <p className="hx-intro">
+          We empower editors to react faster and better to the current issues.
+        </p>
+      </div>
+      <div className="col-5" data-push-left="off-1_xs-0">
+        <p className="product-intro">
+          Editors can create their own dashboard for a thematic search through their own or third-party publications. Google Trends, an article preview and the translation of all articles into English at the push of a button round off our service for the editor of today.
+        </p>
+      </div>
     </div>
   </div>
-</div>
+);
+
+
+const ProductCEP = () => (
+  <div className="l-container">
+    <div className="grid">
+      <div className="col-4">
+        <p className="product-intro">
+          This way you always stay ahead of the competition and profit from the uniquely Axel Springer content. This allows you to quickly and clearly view current content.
+        </p>
+      </div>
+      <div className="col-5" data-push-left="off-1_xs-0">
+        <h2 className="h1 has-cursor has-cursor--400">
+          Content Exchange Platform 
+        </h2>
+        <p className="hx-intro">
+          Get in contact with your colleagues worldwide and exchange content using our platform.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+
+const ProductSSO = () => (
+  <div className="l-container">
+    <div className="grid">
+      <div className="col-4">
+        <h2 className="h1 has-cursor has-cursor--400">
+          Single Sign-on
+        </h2>
+        <p className="hx-intro">
+          We offer all services around registration and login for all customers in the Axel Springer Family.
+        </p>
+      </div>
+      <div className="col-5" data-push-left="off-1_xs-0">
+        <p className="product-intro">
+          This includes newsletter management and automatic login and logout for all linked Axel Springer services.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+
+const ProductPayment = () => (
+  <div className="l-container">
+    <div className="grid">
+      <div className="col-4">
+        <p className="product-intro">
+          Payment and Billing
+        </p>
+      </div>
+      <div className="col-5" data-push-left="off-1_xs-0">
+        <h2 className="h1 has-cursor has-cursor--400">
+          We offer all services for payment and billing for all customers in the Axel Springer Family. 
+        </h2>
+        <p className="hx-intro">
+          Get in contact with your colleagues worldwide and exchange content using our platform.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+
+
+const Products = () => (
+  <div>
+    <ProductNewsfinder />
+    <ProductCEP />
+    <ProductSSO />
+    <ProductPayment />
+  </div>
 );
 
 
