9aa67e741342b04a38d0db4c4d512d9ce6fe784b
Andreas Weber <andreas.weber.1@me.com>
2017-09-13 16:17:37 +0200

    Make home responsive, a bit...

--- src/components/Home/HomeProducts.js
+++ src/components/Home/HomeProducts.js
@@ -9,8 +9,23 @@ import newsfinderMobile2x from "./newsfinder-mobile-2x.png";
 
 const ProductNewsfinder = () => (
   <div className="l-fav-element">
+    <div className="l-container padding-h-lg">
+      <div className="grid">
+        <div className="col-4_xs-12" data-push-left="off-1_xs-0">
+          <h2 className="h1 has-cursor has-cursor--400">
+            Newsfinder
+          </h2>
+          <p className="hx-intro">
+            We empower editors to react faster and better to the current issues.
+          </p>
+          <p className="product-intro">
+            Editors can create their own dashboard for a thematic search through their own or third-party publications. Google Trends, an article preview and the translation of all articles into English at the push of a button round off our service for the editor of today.
+          </p>
+        </div>
+      </div>
+    </div>
     <div className="grid-noGutter-noBottom grid-posAbsolute-bottom">
-      <div className="col-6" data-push-left="off-6">
+      <div className="col-6_xs-12" data-push-left="off-6_xs-0">
         <div className="fav-element__image-section">
           <Picture
             sources = {[
@@ -26,21 +41,6 @@ const ProductNewsfinder = () => (
         </div>
       </div>
     </div>
-    <div className="l-container padding-h-110">
-      <div className="grid">
-        <div className="col-4" data-push-left="off-1_xs-0">
-          <h2 className="h1 has-cursor has-cursor--400">
-            Newsfinder
-          </h2>
-          <p className="hx-intro">
-            We empower editors to react faster and better to the current issues.
-          </p>
-          <p className="product-intro">
-            Editors can create their own dashboard for a thematic search through their own or third-party publications. Google Trends, an article preview and the translation of all articles into English at the push of a button round off our service for the editor of today.
-          </p>
-        </div>
-      </div>
-    </div>
   </div>
 );
 
@@ -48,7 +48,7 @@ const ProductNewsfinder = () => (
 const ProductCEP = () => (
   <div className="l-container">
     <div className="grid">
-      <div className="col-4" data-push-left="off-1_xs-0">
+      <div className="col-4_xs-12" data-push-left="off-1_xs-0">
         <h2 className="h1 has-cursor has-cursor--400">
           Content Exchange Platform 
         </h2>
@@ -59,9 +59,6 @@ const ProductCEP = () => (
           This way you always stay ahead of the competition and profit from the uniquely Axel Springer content. This allows you to quickly and clearly view current content.
         </p>
       </div>
-      <div className="col-5" data-push-left="off-1_xs-0">
-        
-      </div>
     </div>
   </div>
 );
@@ -70,7 +67,7 @@ const ProductCEP = () => (
 const ProductSSO = () => (
   <div className="l-container">
     <div className="grid">
-      <div className="col-4" data-push-left="off-1_xs-0">
+      <div className="col-4_xs-12" data-push-left="off-1_xs-0">
         <h2 className="h1 has-cursor has-cursor--400">
           Single Sign-on
         </h2>
@@ -89,16 +86,13 @@ const ProductSSO = () => (
 const ProductPayment = () => (
   <div className="l-container">
     <div className="grid">
-      <div className="col-4" data-push-left="off-1_xs-0">
+      <div className="col-4_xs-12" data-push-left="off-1_xs-0">
         <h2 className="h1 has-cursor has-cursor--400">
           Payment and Billing 
         </h2>
         <p className="hx-intro">
           We offer all services for payment and billing for all customers in the Axel Springer Family.
         </p>
-      </div>
-      <div className="col-5" data-push-left="off-1_xs-0">
-        
       </div>
     </div>
   </div>
