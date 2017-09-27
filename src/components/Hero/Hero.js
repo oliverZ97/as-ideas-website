c4c109be4c619116039af8935445811450158a84
Andreas Wagner <andreas.wagner@axelspringer.com>
2017-09-27 14:54:59 +0200

    Put main nav items in components, which makes it much easier to add new nav items

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,8 +7,8 @@ export const Hero = (props) => {
   // Component to be wrapped in Hero, given as props
   // Has a simple typecheck if prop is missing, aka undefined
   const Wrapped = props.wrapped !== undefined ? props.wrapped : () => (<div></div>);
-   // Define CSS class depending on prop 'cssClassAddon'
-  const heroCSSClass = props.cssClassAddon !== undefined ? `hero hero--${props.cssClassAddon}` : "hero";
+   // Define CSS class depending on prop 'cssAddon'
+  const heroCSSClass = props.cssAddon !== undefined ? `hero hero--${props.cssAddon}` : "hero";
   
   return (
     <div className={heroCSSClass}>
---
+++ src/components/MainNav/MainNavItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./MainNav.css";
+
+const NavItem = ({ name, link, hasActiveState, cssAddon, pathname }) => {
+  // Is our route active? 
+  const activeRoute = pathname === link;
+  
+  // In case we have a CSS addon we pass it through and add it to our classNames respectively
+  const itemCSSAddon = cssAddon !== undefined ? `main-nav__item--${cssAddon}` : "";
+  const linkCSSAddon = cssAddon !== undefined ? `main-nav__item-link--${cssAddon}` : "";
+  
+  // ActiveState describes wheater our nav items have a visible active state or not
+  const linkCSSActiveState = hasActiveState === false ? "" : `main-nav__item-link--is-active`;
+  
+  return activeRoute ? (
+    <li className={`main-nav__item ${itemCSSAddon}`}>
+      <span className={`main-nav__item-link main-nav__item-link--is-active-route ${linkCSSActiveState} ${linkCSSAddon}`}>
+        {name}
+      </span>
+    </li>
+  ) 
+  :
+  (
+    <li className={`main-nav__item ${itemCSSAddon}`}>
+      <Link 
+        className={`main-nav__item-link ${linkCSSAddon}`} 
+        to={link}
+      >
+        {name}
+      </Link>
+    </li>
+  );
+};
+
+
+export default NavItem;
