16fb68685ce76363d121bace00317c3d19366b22
Thomas Wendzinski <thomas.wendzinski@asideas.de>
2018-08-22 15:48:20 +0200

    Removes old docker login

--- docker_website/build_website.sh
+++ docker_website/build_website.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-DOCKER_USER=$1
-DOCKER_PWD=$2
-
 echo "Copy resources..."
 cp -r ../build .
 
@@ -12,9 +9,6 @@ CURRENT_DATE=$(date +%Y-%m-%d:%H:%M:%S)
 
 rm -f ./build/version.json
 echo "{ \"version\": \"$GO_PIPELINE_COUNTER\", \"date\": \"$CURRENT_DATE\" }" >> ./build/version.json
-
-echo "Build docker-containers"
-docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
 
 docker build --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t "asideas/ideas-website-2:${GO_PIPELINE_COUNTER}" -f Dockerfile .
 
