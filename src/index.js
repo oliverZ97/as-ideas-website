928a9755bc38b3926573c6c898a78a0fa766a438
Sebastian Waschnick <sebastian@waschnick.de>
2018-10-16 16:25:08 +0200

    Kill service worker #2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+(function () {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations()
+      .then(function (registrations) {
+        for (let registration of registrations) {
+          registration.unregister()
+        }
+      })
+  }
+})();
