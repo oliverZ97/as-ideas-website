4a5661b2bfb690f6ba5c2a18f4db9fa866191994
Sebastian Waschnick <sebastian@waschnick.de>
2018-12-28 17:02:23 +0100

    Fixing scp deloying with known hosts

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,14 +12,21 @@ node('oil2') {
       sh "npm install"
     }
 
-    stage('Linting') {
+    stage('Build') {
       sh "npm run build"
+    }
+
+    stage('Optimize') {
+      sh "npm run optimize"
     }
 
     stage('Deploy') {
       // Needs Plugin: https://plugins.jenkins.io/ssh-agent
 //      sshagent(['57d10763-286f-4a9e-bc23-7fd86908196b']) {
-      sh "scp -r ./public/* ec2-user@ec2-18-184-76-12.eu-central-1.compute.amazonaws.com:/usr/share/nginx/html/"
+        // We need to add the host to the list of known hosts, otherwise we will get "Host verification failed"
+        sh ": ssh-keygen -R ec2-18-184-76-12.eu-central-1.compute.amazonaws.com"
+        sh ": \$(ssh-keyscan ec2-18-184-76-12.eu-central-1.compute.amazonaws.com >> ~/.ssh/known_hosts)"
+        sh "scp -r ./public/* ec2-user@ec2-18-184-76-12.eu-central-1.compute.amazonaws.com:/usr/share/nginx/html/"
 //      }
     }
 
